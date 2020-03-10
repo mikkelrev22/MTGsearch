fb4327785c5625d6f91d65ab3be563bc3d96ca44
Michael Thompson <michael.220488@gmail.com>
2020-03-10 16:02:31 -0700

    file: index.js, get req to scryfall api

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
 const express = require('express')
 const app = express()
 const PORT = process.env.port || 42069
+const axios = require('axios')
+
+app.get('/cards/:name', (req, res)=>{
+  console.log(req.params.name)
+  axios.get(`https://api.scryfall.com/cards/named?fuzzy=${req.params.name}`)
+  .then((res)=>{
+    console.log(res)
+  })
+  .catch((error)=>{
+    console.log(error)
+  })
+})
+
+app.use((req, res, next)=>{
+  res.status(404).send('Sorry, I could not find that')
+})
+
+app.use((req, res, next)=>{
+  console.error(err.stack)
+  res.status(500).send('Something broke')
+})
 
 app.listen(PORT, ()=>{
   console.log(`server is now listening on Port ${PORT}`)
