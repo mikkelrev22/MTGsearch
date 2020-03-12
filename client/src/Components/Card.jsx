53749c8693f562d2703ce3bffcdf3d73a15106ad
Michael Thompson <michael.220488@gmail.com>
2020-03-12 15:14:42 -0700

    file: Card.jsx, organized code to make it readable

--- client/src/Components/Card.jsx
+++ client/src/Components/Card.jsx
@@ -3,7 +3,12 @@ import React from 'react'
 const Card = (props) => {
   const cardList = props.cards
   const mappedCards = cardList.map((card, i)=>
-<li key={i}> <div className="nameContainer">{card.name}:</div> <div className="manaContainer">{card.mana_cost}</div><div className="imageContainer"></div><img src={card.image}></img><div className="textContainer">{card.text}</div></li>)
+  <li key={i}> 
+  <div className="nameContainer">{card.name}:</div> 
+  <div className="manaContainer">{card.mana_cost}</div>
+  <div className="imageContainer"><img src={card.image}></img></div>
+  <div className="textContainer">{card.text}</div>
+  </li>)
   return (
     <ul>
       {mappedCards}
